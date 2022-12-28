70f0525c98fc6f053b0118c7c8e2d305c21cad83
Olga <muanaire@gmail.com>
2022-12-28 18:08:35 +0000

    Task10

---
+++ task10/Program.cs
@@ -0,0 +1,29 @@
+﻿// напишите программу, которая выводит случайное число из отрезка [10; 90] 
+//и показывает наибольшую цифру числа
+
+int GetNumberFromRange(int start, int end)
+{
+    int number = new Random().Next(start, end + 1);
+    return number;
+}
+
+int GetMaxDigit(int number)
+{
+    int digit1 = number % 10;
+    int digit2 = number / 10;
+    int max = 0;
+    if (digit1 > digit2 )
+    {
+        max = digit1;
+    }
+    else
+    {
+        max = digit2;
+    }
+    return max;
+}
+
+int randomNumber = GetNumberFromRange(10, 99);
+int maxdigit = GetMaxDigit(randomNumber);
+Console.WriteLine(randomNumber);
+Console.WriteLine($"Для числа {randomNumber} максимальной цифрой является {maxdigit}" );
